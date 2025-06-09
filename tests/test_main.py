4693c002022bfa3bc7e74b949d48c1d6f84db50b
karinchic301 <kar_rash@mail.ru>
2025-06-09 17:44:57 +0300

    Fix test import path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
-from src.main import add
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from main import add
+
 
 def test_add():
     assert add(2, 3) == 5
